de2cb95a7d78b1ef1c3fbeb3ed8acc976dc97147
AntoineNGUYEN31 <50294775+AntoineNGUYEN31@users.noreply.github.com>
2019-07-26 20:22:14 +0200

    Update install_me.sh

--- install_me.sh
+++ install_me.sh
@@ -1,1 +1,10 @@
-yum update
+yum install epel-release
+#install gparted
+yum install gparted
+#in order to read ntfs partitions
+yum install ntfs-3g fuse 
+#torrent downloader
+yum install transmission
+#install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+yum localinstall google-chrome-stable_current_x86_64.rpm
